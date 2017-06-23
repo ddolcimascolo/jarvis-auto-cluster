ffeb0aa839eb8b513a78148dbe2c8f5995826c2b
ddolcimascolo <ddolcimascolo@linagora.com>
2017-06-23 16:17:21 +0200

    Now also supporting trigger changes.

--- systemd/updateHostsFile.sh
+++ systemd/updateHostsFile.sh
@@ -2,8 +2,9 @@
 
 INFO=($(cat -)) # INFO is "<ipv4> <trigger>"
 
-# Remove existing info for that Jarvis
+# Remove existing info for that trigger or IP address
 sudo sed -i "/${INFO[1]}/ d" /etc/hosts
+sudo sed -i "/${INFO[0]}/ d" /etc/hosts
 
 # Add info to the hosts file
 echo "${INFO[0]} ${INFO[1]} # jarvis-auto-cluster $(date +'%s')" | sudo tee -a /etc/hosts
