1abc5d9cd3b45cc530be660ebc0b91222d03abc7
ddolcimascolo <ddolcimascolo@linagora.com>
2017-06-29 18:09:50 +0200

    Now supporting multiple up interfaces on the raspberry.

--- systemd/broadcast.sh
+++ systemd/broadcast.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "$(hostname -I) $(cat /home/pi/jarvis/config/trigger)" | socat - UDP4-DATAGRAM:255.255.255.255:65432,broadcast
+IPS=$(hostname -I)
+
+for ip in ${IPS}
+do
+    echo "${ip} $(cat /home/pi/jarvis/config/trigger)" | socat - UDP4-DATAGRAM:255.255.255.255:65432,broadcast
+done
